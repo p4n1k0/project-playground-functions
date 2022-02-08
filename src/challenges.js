2934e8f19dcec5896d14360999386edd92451889
Tarick <gabre.tarick@gmail.com>
2022-02-08 16:12:05 -0300

    conclui requisito 5

--- src/challenges.js
+++ src/challenges.js
@@ -41,9 +41,14 @@ console.log(concatName(['foguete', 'não', 'tem', 'ré']));
 console.log(concatName(['captain', 'my', 'captain']));
 
 // Desafio 5
-function footballPoints() {
-  // seu código aqui
+function footballPoints(wins, ties) {
+  // seu código aqui;
+  pontos = (wins * 3) + (ties * 1);
+  return pontos; 
 }
+console.log(footballPoints(14, 8));
+console.log(footballPoints(1, 2));
+console.log(footballPoints(0, 0));
 
 // Desafio 6
 function highestCount() {
