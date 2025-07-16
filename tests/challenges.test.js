6659465b97a7d3d12517d2153052f10a6173035e
p4n1k0 <gabre.tarick@gmail.com>
2025-07-16 15:08:31 -0300

    tests p footballPoints

--- tests/challenges.test.js
+++ tests/challenges.test.js
@@ -3,6 +3,7 @@ const {
     calcArea,
     splitSentence,
     concatName,
+    footballPoints,
 } = require('../src/challenges');
 
 describe('1 - Crie uma função usando operador &&', () => {
@@ -60,4 +61,18 @@ describe('4 - Crie uma função que use concatenação de strings', () => {
     it('Retorne \'captain, captain\' quando o parâmetro passado na funcão concatName seja [\'captain\', \'my\', \'captain\']', () => {
         expect(concatName(['captain', 'my', 'captain'])).toBe('captain, captain');
     });
-})
+});
+
+describe('5 - Crie uma função que calcule a quantidade de pontos no futebol', () => {
+    it('Retorne 50 pontos quando o time tenha 14 vitórias e 8 empates', () => {
+        expect(footballPoints(14, 8)).toBe(50);
+    });
+
+    it('Retorne 5 pontos quando o time tenha uma vitória e 2 empates', () => {
+        expect(footballPoints(1, 2)).toBe(5);
+    });
+
+    it('Retorne 0 pontos quando o time tenha 0 vitórias e 0 empates', () => {
+        expect(footballPoints(0, 0)).toBe(0);
+    });
+});
