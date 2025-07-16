f80f57e9332ec247f52bd6ba9ac0d30c95b1aa07
p4n1k0 <gabre.tarick@gmail.com>
2025-07-16 17:51:44 -0300

    tests p funcoes encode e decode

--- tests/challenges.test.js
+++ tests/challenges.test.js
@@ -7,6 +7,8 @@ const {
     highestCount,
     catAndMouse,
     fizzBuzz,
+    encode,
+    decode,
 } = require('../src/challenges');
 
 describe('1 - Função usando operador &&', () => {
@@ -124,4 +126,20 @@ describe('8 - Função fizzBuzz', () => {
     it('Retorne as strings [\'fizz\', \'buzz\'] quando é passado os parâmetros [9, 25] para função fizzBuzz', () => {
         expect(fizzBuzz([9, 25])).toEqual(['fizz', 'buzz']);
     });
-})
+});
+
+describe('9 - Funções que codifica e decodifica', () => {
+    it('Retorne uma string codificada quando a função encode for utilizada', () => {
+        expect(encode('hello')).toBe('h2ll4');
+        expect(encode('How are you today?')).toBe('H4w 1r2 y45 t4d1y?');
+        expect(encode('This is an encoding test.')).toBe('Th3s 3s 1n 2nc4d3ng t2st.');
+        expect(encode('go Tarick!')).toBe('g4 T1r3ck!');
+    });
+
+    it('Retorne uma string decodificada quando a função decode for utilizada', () => {
+        expect(decode('h2ll4')).toBe('hello');
+        expect(decode('H4w 1r2 y45 t4d1y?')).toBe('How are you today?');
+        expect(decode('Th3s 3s 1n 2nc4d3ng t2st.')).toBe('This is an encoding test.');
+        expect(decode('g4 T1r3ck!')).toBe('go Tarick!');
+    });
+});
