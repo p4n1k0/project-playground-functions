cfef9721fb268562c52d91a30aee793dd9d282cc
p4n1k0 <gabre.tarick@gmail.com>
2025-07-16 17:41:41 -0300

    tests p fizzBuzz

--- tests/challenges.test.js
+++ tests/challenges.test.js
@@ -6,6 +6,7 @@ const {
     footballPoints,
     highestCount,
     catAndMouse,
+    fizzBuzz,
 } = require('../src/challenges');
 
 describe('1 - Função usando operador &&', () => {
@@ -110,3 +111,17 @@ describe('7 - Função de caça ao rato', () => {
         expect(catAndMouse(1, 0, 2)).toBe('os gatos trombam e o rato foge');
     });
 });
+
+describe('8 - Função fizzBuzz', () => {
+    it('Retorne as strings [\'bug!\', \'fizzBuzz\', \'bug!\', \'fizz\', \'fizzBuzz\'] quando é passado os parâmetros [2, 15, 7, 9, 45] para função fizzBuzz', () => {
+        expect(fizzBuzz([2, 15, 7, 9, 45])).toEqual(['bug!', 'fizzBuzz', 'bug!', 'fizz', 'fizzBuzz']);
+    });
+
+    it('Retorne as strings [\'bug!\', \'fizz\'] quando é passado os parâmetros [7, 9] para função fizzBuzz', () => {
+        expect(fizzBuzz([7, 9])).toEqual(['bug!', 'fizz']);
+    });
+
+    it('Retorne as strings [\'fizz\', \'buzz\'] quando é passado os parâmetros [9, 25] para função fizzBuzz', () => {
+        expect(fizzBuzz([9, 25])).toEqual(['fizz', 'buzz']);
+    });
+})
