983be18a78cc7fe2e06199704f5d554d3b8faa1e
p4n1k0 <gabre.tarick@gmail.com>
2025-07-16 14:13:38 -0300

    testes p calcArea

--- tests/challenges.test.js
+++ tests/challenges.test.js
@@ -1,4 +1,7 @@
-const { compareTrue } = require('../src/challenges');
+const {
+    compareTrue,
+    calcArea,
+} = require('../src/challenges');
 
 describe('1 - Crie uma função usando operador &&', () => {
     it('Retorne false quando chamar a função compareTrue com um parâmetro de valor false e outro de valor true', () => {
@@ -14,3 +17,17 @@ describe('1 - Crie uma função usando operador &&', () => {
         expect(compareTrue(true, true)).toBe(true);
     });
 });
+
+describe('2 - Crie uma função que calcule a área do triângulo', () => {
+    it('Retorne o valor 250 quando a função calcArea é chamada com o parâmetro base com o valor 10 e o parâmetro height com o valor 50', () => {
+        expect(calcArea(10, 50)).toBe(250);
+    });
+
+    it('Retorne o valor 5 quando a função calcArea é chamada com o parầmetro base com o valor 5 e o parâmetro height com o valor 2', () => {
+        expect(calcArea(5, 2)).toBe(5);
+    });
+
+    it('Retorne o valor 25.5 quando a função calcArea é chamada como parâmetro base com o valor 51 e o parâmetro height com o valor 1', () => {
+        expect(calcArea(51, 1)).toBe(25.5);
+    });
+})
