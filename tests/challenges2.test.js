d4b03765a04e0836e750ebd5439481de139fe4a7
p4n1k0 <gabre.tarick@gmail.com>
2025-07-23 16:46:14 -0300

    testes p triangleCheck

--- tests/challenges2.test.js
+++ tests/challenges2.test.js
@@ -1,4 +1,7 @@
-const { generatePhoneNumber } = require('../src/challenges2');
+const {
+    generatePhoneNumber,
+    triangleCheck,
+} = require('../src/challenges2');
 
 describe('11 - Crie uma função de Número de telefone', () => {
     it('Retorne a string "Array com tamanho incorreto." caso o array tenha o tamanho diferente de 11', () => {
@@ -33,5 +36,25 @@ describe('11 - Crie uma função de Número de telefone', () => {
         expect(generatePhoneNumber([5, 2, 8, 1, 5, 3, 7, 2, 8, 9, 0])).toBe('(52) 81537-2890');
         expect(generatePhoneNumber([0, 2, 4, 3, 5, 5, 6, 8, 9, 2, 1])).toBe('(02) 43556-8921');
         expect(generatePhoneNumber([0, 2, 3, 4, 5, 8, 7, 9, 1, 0, 7])).toBe('(02) 34587-9107');
+    });
+});
+
+describe('12 - Crie uma função de Condição de existência de um triângulo', () => {
+    it('Retorne false quando a medida de qualquer um dos lados seja maior que a soma das medidas dos outros dois', () => {
+        expect(triangleCheck(16, 9, 2)).toBe(false);
+        expect(triangleCheck(10, 3, 4)).toBe(false);
+        expect(triangleCheck(6, 2, 2)).toBe(false);
+    });
+
+    it('Retorne false quando a medida de qualquer um dos lados seja menor que o valor absoluto da diferença entre essas medidas', () => {
+        expect(triangleCheck(10, 13, 2)).toBe(false);
+        expect(triangleCheck(12, 17, 4)).toBe(false);
+        expect(triangleCheck(5, 10, 3)).toBe(false);
+    });
+
+    it('Retorne true quando a medida de qualquer um dos lados seja menor que a soma das medidas dos outros dois e maior que o valor absoluto da diferença entre essas medidas', () => {
+        expect(triangleCheck(10, 14, 8)).toBe(true);
+        expect(triangleCheck(2, 3, 4)).toBe(true);
+        expect(triangleCheck(16, 20, 30)).toBe(true);
     });
 });
