3f669ece1f400c06526d78f752908fb725a32d6d
p4n1k0 <gabre.tarick@gmail.com>
2025-07-23 16:47:56 -0300

    testes p hydrate

--- tests/challenges2.test.js
+++ tests/challenges2.test.js
@@ -1,6 +1,7 @@
 const {
     generatePhoneNumber,
     triangleCheck,
+    hydrate
 } = require('../src/challenges2');
 
 describe('11 - Crie uma função de Número de telefone', () => {
@@ -56,5 +57,15 @@ describe('12 - Crie uma função de Condição de existência de um triângulo',
         expect(triangleCheck(10, 14, 8)).toBe(true);
         expect(triangleCheck(2, 3, 4)).toBe(true);
         expect(triangleCheck(16, 20, 30)).toBe(true);
+    });
+});
+
+describe('13 - Crie uma função de Bem vindo ao Bar da Trybe!', () => {
+    it('Retorne a sugestão de quantos copos de água deve-se beber ao receber uma string', () => {
+        expect(hydrate('1 cerveja')).toBe('1 copo de água');
+        expect(hydrate('1 cachaça, 5 cervejas e 1 copo de vinho')).toBe('7 copos de água');
+        expect(hydrate('2 shots de tequila, 2 cervejas e 1 corote')).toBe('5 copos de água');
+        expect(hydrate('1 copo de catuaba, 1 cervejas e 1 copo de vinho')).toBe('3 copos de água');
+        expect(hydrate('4 caipirinhas e 2 cervejas')).toBe('6 copos de água');
     });
 });
